4167f385ab10ef4a606928277e92178f4d485a11
kermitsmind <kermitsmind@icloud.com>
2021-10-24 18:49:39 +0200

    folder reorganization

--- List 0/l0_t1_1.scala
+++
@@ -1,6 +0,0 @@
-// Definition of main method 'hello'
-@main def hello = println("Hello, world!")
-// To see the result:
-// 1. save the file
-// 2. compile it using 'scala3-compiler'
-// 3. run 'hello' method using 'scala3' hello
