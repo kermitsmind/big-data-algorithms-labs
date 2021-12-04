e46bd3102009742d67fea0aa70111d05ccb96072
kermitsmind <kermitsmind@icloud.com>
2021-12-04 22:48:25 +0100

    l2_t1: sth works

--- List 2/l2_t1/l2_t1.scala
+++ List 2/l2_t1/l2_t1.scala
@@ -1,74 +1,30 @@
+import scala.io.Source
 import scala.collection.concurrent
 import scala.collection.immutable.ListMap
 
-class MapReduce(){
-    def Map(input: List[Tuple2[Int, List[Int]]]): List[Tuple2[Int, Int]] = {
-        var outputList: List[Tuple2[Int, Int]] = List()
-        val output = input.map((node, edges) => edges.map(edge => (edge,node))).flatten
-        // for (row <- input){
-        //     val node: Int = row._1
-        //     val edges: List[Int] = row._2
-        //     if (edges.length != 0){
-        //         for (edge <- edges){
-        //             outputList = outputList :+ (node, edge)
-        //         }
-        //     }else{
-        //         outputList = outputList :+ (node, node)
-        //     }
-        // }
+object MainObject{
+
+     //                                             node, 1
+    def MapFunction(input: Tuple2[Int, Int]): Tuple2[Int, Int] = {
+        val output: Tuple2[Int, Int] = (input._1, 1) // node, 1
         return output
     }
 
-    def Reduce(input: List[Tuple2[Int, Int]]): List[Tuple2[Int, List[Int]]] = {
-        var inversedList: List[Tuple2[Int, Int]] = List()
-        var workingMap: concurrent.Map[Int, List[Int]] = new concurrent.TrieMap()
-        var outputList: List[Tuple2[Int, List[Int]]] = List()
-        for (row <- input){
-            val val1: Int = row._2
-            val val2: Int = row._1
-            if (val1 != val2){
-                if (workingMap.keys.toArray contains val1){
-                    var emptyList: List[Int] = List()
-                    var oldList: List[Int] = workingMap.getOrElse(val1, emptyList)
-                    var newList: List[Int] = oldList :+ val2
-                    workingMap.replace(val1, newList)
-                } else{
-                    workingMap.putIfAbsent(val1, List(val2))
-                }
-            }
-        }
-        for (node <- workingMap.keys){
-            var emptyList: List[Int] = List()
-            var edgesList: List[Int] = workingMap.getOrElse(node, emptyList)
-            outputList = outputList :+ (node, edgesList)
-        }
-        return outputList
-    }
-
-}
-
-object MainObject{
     def main(args: Array[String]) = {
-        var mapReduceObject = new MapReduce()
-        val initialList: List[Tuple2[Int, List[Int]]] = List(
-            (1, List(2,3)),
-            (3, List(1,5)),
-            (2, List(5)),
-            (5, List())
-        )
-        print("\n------: initial list\n")
-        for (row <- initialList){
-            println(row)
+        
+        // import initial list from a file and append to a list variable
+        var initialList: List[Tuple2[Int, Int]] = List()
+        for (line <- Source.fromFile("graph.txt").getLines) {
+            var pairString: Array[String] = line.split(" ")
+            var pair: Tuple2[Int, Int] = (pairString(0).toInt, pairString(1).toInt)
+            initialList = initialList :+ pair
+            // println(initialList)
         }
-        var workingList: List[Tuple2[Int, Int]] = mapReduceObject.Map(initialList)
-        print("\nMap---: working list\n")
-        for (pair <- workingList){
-            println(pair)
-        }
-        val outputList: List[Tuple2[Int, List[Int]]] = mapReduceObject.Reduce(workingList)
-        print("\nReduce: output list\n")
-        for (pair <- outputList){
-            println(pair)
-        }
+
+        // 
+        println("initial list:  " + initialList)
+        println("map:           " + initialList.map(MapFunction))
+        //println("reduce:        " + initialList.map(MapFunction).groupBy(_._1).mapValues(seq => seq.map(_._2).reduce(_ + _)).toMap)
+        println("reduce experiment:        " + initialList.map(MapFunction).groupBy(_._1)     )
     }
 }
