cfcf050e5b9b7e5dd4af5bbb97dbdea91b69ef58
kermitsmind <kermitsmind@icloud.com>
2021-12-07 19:49:27 +0100

    Pre .gitignore changes

--- List 2/l2_t1/l2_t1.scala
+++ List 2/l2_t1/l2_t1.scala
@@ -4,27 +4,27 @@ import scala.collection.immutable.ListMap
 
 object MainObject{
 
-     //                                             node, 1
-    def MapFunction(input: Tuple2[Int, Int]): Tuple2[Int, Int] = {
-        val output: Tuple2[Int, Int] = (input._1, 1) // node, 1
+    def MapFunction(input: String): Tuple2[Int, Int] = {
+        var pairString: Array[String] = input.split(" ")
+        var node1: Int = pairString(0).toInt
+        var node2: Int = pairString(1).toInt
+        val output: Tuple2[Int, Int] = (node1, node2)
+        return output
+    }
+
+    def ReduceFunction(input: Tuple2[Int, Int]): Int = {
+        val output: Int = input._1 + input._2
         return output
     }
 
     def main(args: Array[String]) = {
         
-        // import initial list from a file and append to a list variable
-        var initialList: List[Tuple2[Int, Int]] = List()
-        for (line <- Source.fromFile("graph.txt").getLines) {
-            var pairString: Array[String] = line.split(" ")
-            var pair: Tuple2[Int, Int] = (pairString(0).toInt, pairString(1).toInt)
-            initialList = initialList :+ pair
-            // println(initialList)
-        }
-
-        // 
-        println("initial list:  " + initialList)
-        println("map:           " + initialList.map(MapFunction))
-        //println("reduce:        " + initialList.map(MapFunction).groupBy(_._1).mapValues(seq => seq.map(_._2).reduce(_ + _)).toMap)
-        println("reduce experiment:        " + initialList.map(MapFunction).groupBy(_._1)     )
+        var initList = Source.fromFile("graph.txt").getLines
+        println("initial list:  " + initList.toList)
+        var mapList = Source.fromFile("graph.txt").getLines.map(MapFunction)
+        println("map:           " + mapList.toList)
+        // println("reduce:        " + initialList.map(MapFunction).groupBy(_._1).mapValues(seq => seq.map(_._2).reduce(_ + _)).toMap) // sth works
+        //var reduceList = Source.fromFile("graph.txt").getLines.map(MapFunction).reduce(ReduceFunction)
+        //println("reduce:        " + reduceList.toList)
     }
 }
