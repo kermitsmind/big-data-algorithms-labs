c6616114c5bc37dbfcf624239526298d1e5f1b10
kermitsmind <kermitsmind@icloud.com>
2022-01-13 10:09:28 +0100

    l3_t1: crawling and returning next link to crawl works

--- List 3/l3_t1.scala
+++ List 3/l3_t1.scala
@@ -3,6 +3,9 @@
     // https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
     // https://stackoverflow.com/questions/2013124/regex-matching-up-to-the-first-occurrence-of-a-character
     // https://stackoverflow.com/questions/2078915/a-regular-expression-to-exclude-a-word-string
+    // https://www.geeksforgeeks.org/scala-string-replace-method-with-example/
+    // https://alvinalexander.com/scala/how-to-generate-random-numbers-characters-sequences-in-scala/
+    // https://www.tutorialspoint.com/scala/scala_arrays.htm
 
 
 import scala.io.Source
@@ -15,15 +18,29 @@ import scala.util.matching.Regex
 
 object MainObject{
 
-    // takes url and return string array of wiki/Topic hrefs
-    def crawlPageFromWeb(url: String): Array[String] = {
+    // takes name, string array and saves to file with given name
+    def writeFile(fileName: String, stringArray: Array[String]): Unit = {
+        val fileWriter = new FileWriter(fileName, true)
+        for (line <- stringArray){
+            fileWriter.write(line)
+            fileWriter.write("\n")
+        }
+        fileWriter.close()
+    }
+
+    // takes url, obtain wiki/Topic hrefs and save them to file, returns one link out of the found ones (randomly)
+    def crawlPageFromWeb(url: String): String = {
         val pageContent = Source.fromURL("https://en.wikipedia.org/wiki/Web_crawler").mkString
+        val pageName = "wiki\\/[^\"]*".r.findAllIn(url).next.replace('/', '_')
         // below regex mathes many url's however on wikipedia wikipedia's subpages are linked via 'wiki/Topic' thus below regex won't work as desired
         // val generalHrefPattern = "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})".r
         val wikiHrefPattern = "wiki\\/(?!.*%|Category|Help|Wikipedia|Special|" +
           "Talk|Privacy_policy|Cookie_statement|Terms_of_Use|Portal|Main_Page|File|Template)[^\"]*"
         val hrefs = wikiHrefPattern.r.findAllIn(pageContent).toSet.toArray
-        return hrefs
+        writeFile(fileName = pageName, stringArray = hrefs)
+        val randomNumberGenerator = scala.util.Random
+        val randomNumber = randomNumberGenerator.nextInt(hrefs.length)
+        return ("https://en.wikipedia.org/" + hrefs(randomNumber))
     }
 
 
@@ -31,8 +48,8 @@ object MainObject{
     // main function
     def main(args: Array[String]): Unit = {
         val html = "https://en.wikipedia.org/wiki/Web_crawler"
-        val testArray = crawlPageFromWeb(html)
-        for (a <- testArray){println(a)}
+        val nextLink: String = crawlPageFromWeb(html)
+        println(nextLink)
     }   
 
 }
