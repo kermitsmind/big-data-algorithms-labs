977c6d68ca50c19a6df71a56d90ab453c2c568b2
kermitsmind <kermitsmind@icloud.com>
2022-01-14 12:30:53 +0100

    l3_t1: web page files and web page links have the same format but sth does nor work

--- List 3/l3_t1/l3_t1.scala
+++ List 3/l3_t1/l3_t1.scala
@@ -12,6 +12,7 @@
     // https://www.baeldung.com/scala/find-index-element-in-list
     // https://alvinalexander.com/scala/how-to-format-numbers-commas-international-currency-in-scala/
     // https://en.wikipedia.org/wiki/Matrix_multiplication_algorithm
+    // https://www.codegrepper.com/code-examples/java/how+to+remove+first+character+from+string+in+scala
 
 
 import scala.io.Source
@@ -38,27 +39,27 @@ object MainObject{
     // take url, obtain wiki/Topic hrefs and save them to file, returns one link out of the found ones (randomly)
     def crawlPageFromWeb(url: String): String = {
         val pageContent = Source.fromURL(url).mkString
-        val pageName = "wiki\\/[^\"]*".r.findAllIn(url).next.replace('/', '_')
+        val pageName = "(?<=https:\\/\\/en.wikipedia.org\\/wiki\\/).*".r.findAllIn(url).next
         // below regex mathes many url's however on wikipedia wikipedia's subpages are linked via 'wiki/Topic' thus below regex won't work as desired
         // val generalHrefPattern = "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})".r
-        val wikiHrefPattern = "wiki\\/(?!.*%|Category|Help|Wikipedia|Special|" +
+        val wikiHrefPattern = "(?<=wiki\\/)(?!.*%|Category|Help|Wikipedia|Special|" +
         "Talk|Privacy_policy|Cookie_statement|Terms_of_Use|Portal|Main_Page|File|Template)[^\"]*"
         val hrefs = wikiHrefPattern.r.findAllIn(pageContent).toSet.toArray
         writeFile(fileName = ("webPages/" + pageName), stringArray = hrefs)
         val randomNumberGenerator = scala.util.Random
         val randomNumber = randomNumberGenerator.nextInt(hrefs.length)
-        return ("https://en.wikipedia.org/" + hrefs(randomNumber))
+        return ("https://en.wikipedia.org/wiki/" + hrefs(randomNumber))
     }
 
     // take initial web page url and number n, crawl n pages and save them to files
     def crawlGivenNumberOfPages(url: String, n: Int): Unit = {
         val crawledPagesListName: String = "crawledPagesList"
         var crawledPagesListArray: Array[String] = Array()
-        crawledPagesListArray = crawledPagesListArray.appended(url)
+        crawledPagesListArray = crawledPagesListArray.appended("(?<=https:\\/\\/en.wikipedia.org\\/wiki\\/).*".r.findAllIn(url).next)
         var nextUrlToVisit: String = crawlPageFromWeb(url = url)
         var remainingPagesToVisit: Int = n -1
         for (i <- 1 to remainingPagesToVisit){
-            crawledPagesListArray = crawledPagesListArray.appended(nextUrlToVisit)
+            crawledPagesListArray = crawledPagesListArray.appended("(?<=https:\\/\\/en.wikipedia.org\\/wiki\\/).*".r.findAllIn(nextUrlToVisit).next)
             nextUrlToVisit = crawlPageFromWeb(url = nextUrlToVisit)
         }
         writeFile(fileName = crawledPagesListName, stringArray = crawledPagesListArray)
@@ -161,7 +162,7 @@ object MainObject{
     // main function
     def main(args: Array[String]): Unit = {
         val html = "https://en.wikipedia.org/wiki/Web_crawler"
-        // crawlGivenNumberOfPages(url = html, n = 10)
+        crawlGivenNumberOfPages(url = html, n = 3)
         // val rTest: Array[String] = readFile(fileName = "crawledPagesList")
         // for (line <- rTest){
         //     println(line)
